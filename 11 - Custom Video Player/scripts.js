2085d92da36d68bb177e69e851e5104339d04187
Andrea Lecuona <andre.lecuona91@gmail.com>
2021-01-20 10:52:24 -0300

    Completo undecimo dia. Agrego archivos accesorios js al gitignore

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,86 @@
+const player = document.querySelector('.player');
+
+const video = player.querySelector('.viewer');
+
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+const fullscreen = player.querySelector('.fullscreen');
+
+
+
+function togglePlay(){
+    if(video.paused){
+        video.play();
+    } else {
+        video.pause();
+    }
+
+};
+
+
+function updateButton(){
+    const icon = this.paused ? '►' : '❚❚';
+    toggle.textContent = icon;
+};
+
+
+function skip(){
+    video.currentTime += parseFloat(this.dataset.skip);
+}
+
+
+function handleRangeUpdate(){
+    video[this.name] = this.value;
+};
+
+
+function handleProgress(){
+    const percent = (video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`;
+};
+
+
+function scrub(e){
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
+    
+    const scrubPercent = (scrubTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${scrubPercent}%`;
+}
+
+function goFullSize() {
+    if (!document.fullscreenElement) {
+        player.requestFullscreen();
+    } else {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        }
+    }
+};
+
+
+
+video.addEventListener('click', togglePlay);
+toggle.addEventListener('click', togglePlay);
+
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+
+skipButtons.forEach(button => button.addEventListener('click', skip));
+
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+
+video.addEventListener('timeupdate', handleProgress);
+
+progress.addEventListener('click', scrub);
+let mousedown = false;
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
+
+fullscreen.addEventListener('click', goFullSize);
