917ee5063944e5ac2a27f14c5ecbae5ca399a8ae
Andrea Lecuona <andre.lecuona91@gmail.com>
2021-02-09 16:39:28 -0300

    Completo vigesimo cuarto dia

---
+++ 24 - Sticky Nav/script.js
@@ -0,0 +1,19 @@
+const nav = document.querySelector('#main');
+const topOfNav = nav.offsetTop;
+
+function fixNav() {
+    // console.log(topOfNav, window.scrollY);
+
+    if (window.scrollY >= topOfNav) {
+
+        document.body.classList.add('fixed-nav');
+
+        document.body.style.paddingTop = nav.offsetHeight;
+
+    } else {
+        document.body.style.paddingTop = 0;
+        document.body.classList.remove('fixed-nav');
+    }
+}
+
+window.addEventListener('scroll', fixNav);
