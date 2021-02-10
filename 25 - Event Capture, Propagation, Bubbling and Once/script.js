6fda728db62a31196ebbda6da6fb53fc928675cd
Andrea Lecuona <andre.lecuona91@gmail.com>
2021-02-10 15:16:36 -0300

    Completo vigesimo quinto dia

---
+++ 25 - Event Capture, Propagation, Bubbling and Once/script.js
@@ -0,0 +1,16 @@
+const divs = document.querySelectorAll('div');
+const button = document.querySelector('button');
+
+function logText(e) {
+    console.log(this.classList.value);
+
+    e.stopPropagation();
+
+}
+
+divs.forEach(div => div.addEventListener('click', logText, {
+    capture: false,
+    once: true
+}));
+
+button.addEventListener('click', () => console.log('I got clicked!'), { once: true });
